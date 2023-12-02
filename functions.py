e11738379d45e5ef815a7d61698da9f19b8f94af
sairaj777 <sairajpauj@gmail.com>
2023-12-02 19:14:40 +1000

    added the function to test shadow sentence. Modified the dublicate testor function as it was not working as expected.

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import string
 week = ["tuesday", "wednesday", "thursday", "friday", "saturday", "sunday", "monday" ]
 
 def daycalculator(date):
@@ -5,10 +6,26 @@ def daycalculator(date):
     dates = range(7)
     week_dictionary = dict(zip(dates, week))
     return week_dictionary[remainder]
-def duplicateTestor(importedString):               # function to test sentence duplicates
-    if (len(importedString) == len(set(importedString))):
-        return False
+
+def duplicateTestor(importedString): # function to test  duplicate letters in words
+    for words in importedString:
+        if len(words) > len(set(words)):
+            return True
+            break
     else:
-        return True
+        return False
+
+
+def shadowSentenceTest(sentence1,sentence2): # function to test shadow sentences
+    if (len(sentence1) == len(sentence2)):
+        for index, word in enumerate(sentence1):
+            if len(set(sentence1[index]).intersection(set(sentence2[index]))) > 1:
+                return False
+                break
+            else:
+                return True
+    else:
+        return False
+
 
 
--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 # Task:  Loop though days of a month and print Friyay when the day is Friday. From Wed to Thrusday you need to print 'Almost there 🙂'
-
-
 from functions import *
 from class_file import Car
 from random import choice
 import socket
+import datetime
 
+# find dns from ip address
 
 hostname = socket.gethostbyaddr(input("provide the ip address: "))[0]
 print (f"the host name is {hostname}")
 
-duplicateString =duplicateTestor( list(input("enter the string: ").split())) # importing the sentence to test duplicates
-print (duplicateString)
+# task task to find duplicate letters in words
 
+current_datetime = datetime.datetime.now()
+duplicateString =duplicateTestor( list(input("enter the string: ").split())) # importing the sentence to test duplicates
+#print (duplicateString)
+
+file = open("File_Duplicate_String",'a')
+file.write(f"{current_datetime}: {str(duplicateString)}\n")
+
+# Shadow sentence task
+
+sentence1 = list(input("enter the 1st sentence: ").split())
+sentence2 = list(input("enter the 2nd sentence: ").split())
+shadowSentence = shadowSentenceTest(sentence1, sentence2)
+print(f"the sentences are shadow sentence: {shadowSentence}")
+
+# Car task
 
 lancer = Car("lancer","mistibushi",2016)
 jazz = Car("jazz","honda",2004)
 swift = Car("swift","maruthi",2011)
-
-
-
 
 if (__name__ == "__main__" ):
     date = int(input ("Enter the date of the month: ")) #date variable
