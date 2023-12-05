7a3a385452432554699815b93c25876348c93c90
sairaj777 <57239434+sairaj777@users.noreply.github.com>
2023-12-05 17:24:21 +1000

    Update everydaycode.py
    Socket programming using python

--- everydaycode.py
+++ everydaycode.py
@@ -1,1 +1,36 @@
-pruint ("This is a file reated in GitHub")
+#server side code
+
+import socket
+
+server = socket.socket()
+print("socket created")
+
+server.bind(("localhost",9999))
+
+server.listen(3) #listen upto 3 hosts
+
+print("waiting for connections")
+
+while True:
+    client,address = server.accept() #accept the ip address and client socket
+
+    name =  client.recv((1024)).decode()
+    print("connected with", address , name)
+    client.send(bytes("welcome to rajNetworks","utf-8"))
+    client.close()
+
+#
+
+#client side code
+
+import socket
+
+client = socket.socket()
+
+client.connect(("localhost", 9999))
+
+name = input("enter your name: ")
+
+client.send(bytes(name, "utf-8"))
+
+print(client.recv(1024).decode())
